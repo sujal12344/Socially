1bf2e173ba27b681a59693426df04ef0e52fc435
sujal <sujalkesharwani220@gmail.com>
2025-04-06 18:32:11 +0530

    create post component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+// import { getPosts } from "@/actions/post.action";
+// import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+// import PostCard from "@/components/PostCard";
+// import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  // const posts = await getPosts();
+  // const dbUserId = await getDbUserId();
+
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-    </header>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {/* {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))} */}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        {/* <WhoToFollow /> */}
+      </div>
+    </div>
   );
 }
