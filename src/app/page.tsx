bd3ad38187584717660c23bdca2f7e47fccc53f3
sujal <sujalkesharwani220@gmail.com>
2025-04-06 21:16:17 +0530

    home page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-// import { getPosts } from "@/actions/post.action";
-// import { getDbUserId } from "@/actions/user.action";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
-// import PostCard from "@/components/PostCard";
+import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
-  // const posts = await getPosts();
-  // const dbUserId = await getDbUserId();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
@@ -16,9 +16,9 @@ export default async function Home() {
         {user ? <CreatePost /> : null}
 
         <div className="space-y-6">
-          {/* {posts.map((post) => (
+          {posts.map((post) => (
             <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-          ))} */}
+          ))}
         </div>
       </div>
 
