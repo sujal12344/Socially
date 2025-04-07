127c1e11fcade09ae1b1b1ba714057427a323e49
sujal <sujalkesharwani220@gmail.com>
2025-04-07 12:55:30 +0530

    comment

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -25,9 +25,9 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       try {
         // This code RUNS ON YOUR SERVER after upload
-        console.log("Upload complete for userId:", metadata.userId);
+        // console.log("Upload complete for userId:", metadata.userId);
 
-        console.log("file url is", file.ufsUrl);
+        // console.log("file url is", file.ufsUrl);
 
         // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
         return { uploadedBy: metadata.userId };
