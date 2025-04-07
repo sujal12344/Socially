07089515d6b7a1ce5ebcfeb18a7e3ed5ab0efe0f
sujal <sujalkesharwani220@gmail.com>
2025-04-07 12:31:18 +0530

    core setup

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,42 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      // maxFileCount: 1,
+      // minFileCount: 1,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const { userId } = await auth();
+      if (!userId) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      try {
+        // This code RUNS ON YOUR SERVER after upload
+        console.log("Upload complete for userId:", metadata.userId);
+
+        console.log("file url is", file.ufsUrl);
+
+        // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+        return { uploadedBy: metadata.userId };
+      } catch (error) {
+        // Handle error (e.g., log it, notify someone, etc.)
+        console.error("Error in onUploadComplete:", error);
+        throw new UploadThingError("Upload failed");
+      }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
