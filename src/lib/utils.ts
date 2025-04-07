bf3887dc602f17e5de1c1035e776fcd80ca23e05
sujal <sujalkesharwani220@gmail.com>
2025-04-08 03:26:53 +0530

    fun created for formatUrlForDisplay

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatUrlForDisplay(url: string): string {
+  let formatted = url.replace(/^https?:\/\//, "");
+  if (formatted.endsWith("/")) formatted = formatted.slice(0, -1);
+  return formatted;
 }
